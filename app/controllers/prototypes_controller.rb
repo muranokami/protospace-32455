3f372d913fcbc4e13efa96ab37ae316d8639f8c4
muranokami <pikupiku68574.dj@gmail.com>
2020-12-14 18:19:09 +0900

    投稿機能を実装しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -10,19 +10,20 @@ class PrototypesController < ApplicationController
   end
 
   def create 
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else  
+      render :new
+    end
   end
 
-  def destroy
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def show
-  end
+  
 
   def move_to_index
     unless user_signed_in?
