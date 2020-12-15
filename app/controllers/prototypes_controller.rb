889ecbf2560ddab77284467d461eddd72f1a62c4
muranokami <pikupiku68574.dj@gmail.com>
2020-12-15 21:29:57 +0900

    Commentモデルおよびテーブルを作成しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,4 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototypes, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show]
   def index
     @prototypes = Prototype.all
@@ -18,8 +17,31 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+      prototype.destroy
+      redirect_to root_path
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.Includes(:user)
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+      prototype.update(prototype_params)
+      if 
+        redirect_to root_path
+      else
+        render :edit
+      end 
+    end
   end
 
   private
@@ -27,11 +49,8 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  
-
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: :index
-  resources :prototypes, only: :new
-  resources :prototypes, only: :show
-  resources :prototypes do
+  resources :prototypes, only: [:index, :new, :create, :destroy, :show, :edit, :update] do
     resources :comments, only: :create
-    collection do
-    end
   end
 end
  
